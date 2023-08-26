496b507a77f521f2c23555046b306e3ecca2d1ee
Joey Trampush <j.trampush@gmail.com>
2023-08-26 14:34:20 -0700

    more updates

--- _common.R
+++ _common.R
@@ -6,7 +6,7 @@ knitr::opts_chunk$set(
   # cache = TRUE,
   fig.retina = 2,
   fig.width = 6,
-  fig.asp = 2/3,
+  fig.asp = 2 / 3,
   fig.show = "hold"
 )
 
@@ -23,33 +23,4 @@ options(
   width = 77 # 80 - 3 for #> comment
 )
 
-ggplot2::theme_set(ggplot2::theme_gray(12))
-
-# use results: "asis" when setting a status for a chapter
-status <- function(type) {
-  status <- switch(type,
-    polishing = "should be readable but is currently undergoing final polishing",
-    restructuring = "is undergoing heavy restructuring and may be confusing or incomplete",
-    drafting = "is currently a dumping ground for ideas, and we don't recommend reading it",
-    complete = "is largely complete and just needs final proof reading",
-    stop("Invalid `type`", call. = FALSE)
-  )
-
-  class <- switch(type,
-    polishing = "note",
-    restructuring = "important",
-    drafting = "important",
-    complete = "note"
-  )
-
-  cat(paste0(
-    "\n",
-    ":::: status\n",
-    "::: callout-", class, " \n",
-    "You are reading the work-in-progress second edition of R for Data Science. ",
-    "This chapter ", status, ". ",
-    "You can find the complete first edition at <https://r4ds.had.co.nz>.\n",
-    ":::\n",
-    "::::\n"
-  ))
-}
+# ggplot2::theme_set(ggplot2::theme_gray(12))
